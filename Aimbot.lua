8cf9bf2b7b26b6680813fe6dfcc886cc8f63b94e
clgtmm <103909534+clgtmm@users.noreply.github.com>
2023-08-25 20:26:01 +0700

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,20 +1,13 @@
-spawn(function()
-        local gg = getrawmetatable(game)
-        local old = gg.__namecall
-        setreadonly(gg,false)
-        gg.__namecall = newcclosure(function(...)
-            local method = getnamecallmethod()
-            local args = {...}
-            if tostring(method) == "FireServer" then
-                if tostring(args[1]) == "RemoteEvent" then
-                    if tostring(args[2]) ~= "true" and tostring(args[2]) ~= "false" then
-                        if _G.Setting["Start Hunting"] and _G.PosTar ~= nil then
-                            args[2] = _G.PosTar
-                            return old(unpack(args))
-                        end
-                    end
-                end
-            end
-            return old(...)
-        end)
-end)
+
+--[[Hello xin chao cac ban dumper hoac dai loai vay minh la skidder so 1 viet nam ok nha]]
+local AimBotCaiDitMeMay = nil
+local Mouse = game.Players.LocalPlayer:GetMouse()
+AimBotCaiDitMeMay = hookmetamethod(game, "__index", newcclosure(function(self, Index)
+    if self == Mouse and not checkcaller() and GetClosestPlayer() then
+	local HitPart = GetClosestPlayer()
+        if Index == "Hit" or Index == "hit" then
+            return HitPart.CFrame
+        end
+    end
+    return AimBotCaiDitMeMay(self, Index)
+end))
